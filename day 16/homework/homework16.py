bbcc65ea66203d83da897e57b09fb009519f59c0
andrewjanoashvili <andriajanoashvili@gmail.com>
2025-11-01 10:23:16 +0400

    second commit

---
+++ day 16/homework/homework16.py
@@ -0,0 +1,38 @@
+i = 0
+while i <= 10:
+    print(i)
+    i += 1
+
+
+
+    n = 30
+while n >= 1:
+    print(n)
+    n -= 1
+
+
+
+    i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+
+
+i = 1
+while i <= 100:
+    print(i)
+    i += 2
+
+
+i = 1
+while i <= 100:
+    print(i)
+    i += 3
+
+
+i = 1
+while i <= 50:
+    print(i)
+    i += 3
+    
